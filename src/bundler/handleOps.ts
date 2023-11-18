18db88c00616880514b2cbeb0e54315ab674de76
Mekyle Naidoo <mekylenaidoo1@gmail.com>
2023-11-18 07:44:25 +0300

    Handleops function

--- src/bundler/handleOps.ts
+++ src/bundler/handleOps.ts
@@ -8,12 +8,15 @@ import {
   createWalletClient,
   custom,
   stringify,
-  Hex
+  Hex,
+  concatHex,
+  encodeFunctionData
 } from 'viem'
 import { privateKeyToAccount } from 'viem/accounts' 
 
 import { entrypointAbi } from '../bundler/abi'
 import { goerli } from 'viem/chains'
+import { SimpleAccountFactoryAbi } from './simpleAccountFactory'
 
 const publicClient = createPublicClient({
   chain: goerli,
@@ -26,15 +29,49 @@ const entrypointContract = getContract({
   publicClient,
 })
 
-console.log(entrypointContract)
-
-
-
-console.log("Hey test 1")
 if (!process.env.PRIVATE_KEY) throw new Error("PRIVATE_KEY environment variable not set")
 const account = privateKeyToAccount(process.env.PRIVATE_KEY as Hex) 
 
-console.log(account)
+const walletClient = createWalletClient({
+  account: account,
+  chain: goerli,
+  transport: http(),
+})
+
+const initCode = concatHex(
+  [
+    '0x9406Cc6185a346906296840746125a0E44976454',
+     encodeFunctionData({abi: SimpleAccountFactoryAbi, functionName: 'createAccount', args:[walletClient.account.address, 0n]})
+  ])
+
+try {
+  const { result } = await publicClient.simulateContract({
+    address: '0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789',
+    abi: entrypointAbi,
+    functionName: 'getSenderAddress',
+    args: [initCode],
+    account,
+  })
+    // some code that may throw an error
+  } catch (err) {
+    if (err instanceof Error) {
+      const revertError = err.walk(err => err instanceof ContractFunctionRevertedError)
+      if (revertError instanceof ContractFunctionRevertedError) {
+        const errorName = revertError.data?.errorName ?? ''
+        // do something with `errorName`
+      }
+    }
+  }
+
+const { result } = await publicClient.simulateContract({
+    address: '0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789',
+    abi: entrypointAbi,
+    functionName: 'getSenderAddress',
+    args: [initCode],
+    account,
+  })
+
+  
 
 // Entrypoint contract ABi
 
@@ -44,10 +81,7 @@ console.log("Hey I'm here")
 // Create a signer
 
 
-const walletClient = createWalletClient({
-  chain: goerli,
-  transport: http(),
-})
+
 
 // 1. Create contract instance
 
@@ -55,6 +89,23 @@ const walletClient = createWalletClient({
 /*
 1. Entrypoint contract( getContract())
 2. Able to write the the entrypoint contract
+3. Simulate contract which is an entrypoint contract
 3. UserOp object for the handleOps function with all other parameters
+{
+  sender: ,
+  nonce:,
+  initCode:,
+  callData:,
+  paymasterAndData:,
+  maxFeePerGas:,
+  maxPriorityFeePerGas:,
+  callGasLimit:,
+  verificationGasLimit:,
+  preVerificationGas:,
+  signature:,
+}
+
+
+
 4. Send the transaction
 */
